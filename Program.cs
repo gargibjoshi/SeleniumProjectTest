145952890fba6feb6320fc3ae6376f3e7b3d9734
gargi joshi <gargijoshi12@gmail.com>
2018-06-04 01:10:14 +0530

    New update

--- Program.cs
+++ Program.cs
@@ -13,17 +13,31 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //HtmlDocument document2 = new HtmlDocument();
-           // document2.Load(@"C:\Temp\sample.txt");
+            // document2.Load(@"C:\Temp\sample.txt");
 
-            var url = "http://http://executeautomation.com/demosite/Login.html";
+            var url = "http://executeautomation.com/demosite/Login.html";
             var web = new HtmlWeb();
             var document2 = web.Load(url);
 
-            HtmlNode[] nodes = document2.DocumentNode.SelectNodes(".//*").ToArray();
-            foreach (HtmlNode item in nodes)
+            /* select all nodes within the particular tag
+             HtmlNode[] nodes = document2.DocumentNode.SelectNodes(".//form/*").ToArray();
+               foreach (HtmlNode item in nodes)
+               {
+                   Console.WriteLine(item.InnerHtml);
+               }
+               */
+
+            //select all iput nodes
+            HtmlNode docNode = document2.DocumentNode;
+            HtmlNodeCollection nodes = docNode.SelectNodes("//input"); //SelectNodes takes a XPath expression
+            foreach (HtmlNode node in nodes)
             {
-                Console.WriteLine(item.InnerHtml);
+                String id = node.GetAttributeValue("id", "NoId");    // Fetch id of HTML element
+                String name = node.GetAttributeValue("name", "NoName");  // Fetch parameter name (GET/POST)
+                String type = node.GetAttributeValue("type", "NoType");  // Fetch type of input element
+                String xPath = node.XPath;                                   
             }
+
 
             //read under specific node
             /* HtmlNode[] nodes = document2.DocumentNode.SelectNodes("//a").ToArray();
